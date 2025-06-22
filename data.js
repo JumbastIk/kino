481ee2fd95039dca3537f70f44eb11695bfdc51d
JumbastIk <142997161+JumbastIk@users.noreply.github.com>
2025-06-22 18:20:51 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -22,5 +22,61 @@ const movies = [
     poster: 'https://kinozonee.b-cdn.net/thumbnails/comedy1.jpg',
     category: 'Комедия',
     videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'comedy2',
+    title: 'Комедия 2',
+    desc: 'Еще одна веселая история.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/comedy2.jpg',
+    category: 'Комедия',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'action1',
+    title: 'Боевик 1',
+    desc: 'Динамичный боевик с захватывающим сюжетом.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/action1.jpg',
+    category: 'Боевик',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'action2',
+    title: 'Боевик 2',
+    desc: 'Вторая часть с новыми героями.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/action2.jpg',
+    category: 'Боевик',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'drama1',
+    title: 'Драма 1',
+    desc: 'Глубокая эмоциональная драма.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/drama1.jpg',
+    category: 'Драма',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'drama2',
+    title: 'Драма 2',
+    desc: 'Новая история о любви и жизни.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/drama2.jpg',
+    category: 'Драма',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'fantasy1',
+    title: 'Фэнтези 1',
+    desc: 'Магический мир и эпические приключения.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/fantasy1.jpg',
+    category: 'Фэнтези',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
+  },
+  {
+    id: 'fantasy2',
+    title: 'Фэнтези 2',
+    desc: 'Продолжение сказочной саги.',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/fantasy2.jpg',
+    category: 'Фэнтези',
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
   }
 ];
