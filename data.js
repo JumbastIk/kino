a58f0eea79c90f8bd8cabbe45d185e3257f7f0fc
JumbastIk <142997161+JumbastIk@users.noreply.github.com>
2025-06-22 18:16:42 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -3,88 +3,24 @@ const movies = [
     id: 'interstellar',
     title: 'Интерстеллар',
     desc: 'Фантастика, 2014. Космическое путешествие...',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/interstellar.jpg',
     category: 'Драма',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
   },
   {
     id: 'tenet',
     title: 'Довод',
     desc: 'Триллер, 2020. Тайм-менеджмент и спецоперации...',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/tenet.jpg',
     category: 'Драма',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
   },
   {
     id: 'comedy1',
     title: 'Комедия 1',
     desc: 'Легкая комедия для поднятия настроения.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
+    poster: 'https://kinozonee.b-cdn.net/thumbnails/comedy1.jpg',
     category: 'Комедия',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'comedy2',
-    title: 'Комедия 2',
-    desc: 'Еще одна веселая история.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Комедия',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'action1',
-    title: 'Боевик 1',
-    desc: 'Динамичный боевик с захватывающим сюжетом.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Боевик',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'action2',
-    title: 'Боевик 2',
-    desc: 'Вторая часть с новыми героями.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Боевик',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'drama1',
-    title: 'Драма 1',
-    desc: 'Глубокая эмоциональная драма.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Драма',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'drama2',
-    title: 'Драма 2',
-    desc: 'Новая история о любви и жизни.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Драма',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'comedy3',
-    title: 'Комедия 3',
-    desc: 'Свежий взгляд на комедийный жанр.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Комедия',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'fantasy1',
-    title: 'Фэнтези 1',
-    desc: 'Магический мир и эпические приключения.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Фэнтези',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
-  },
-  {
-    id: 'fantasy2',
-    title: 'Фэнтези 2',
-    desc: 'Продолжение сказочной саги.',
-    poster: 'https://kinozonee.b-cdn.net/thumbnail_e903b510.jpg',
-    category: 'Фэнтези',
-    videoUrl: 'https://kinozonee.b-cdn.net/playlist.m3u8'
+    videoUrl: 'https://vz-0818f41b-230.b-cdn.net/4813b13a-ba29-4da5-a2c5-140ab5305172/playlist.m3u8'
   }
 ];
