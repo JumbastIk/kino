34a2b47b47ceedbeef3cdf286bc10b04d640649d
JumbastIk <142997161+JumbastIk@users.noreply.github.com>
2025-07-08 18:11:56 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = 'https://cmworinijkexswnjdhao.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtd29yaW5pamtleHN3bmpkaGFvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDUwNTQ5NywiZXhwIjoyMDY2MDgxNDk3fQ.pyZ5ldHVeBNT6szVPPBC5Tg3HPBWxbV1uuBpBCFE0mo';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
